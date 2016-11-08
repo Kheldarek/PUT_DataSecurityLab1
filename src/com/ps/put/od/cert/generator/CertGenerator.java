f9e308d6ed494d0d214dfc342aca5b64a35a08d9
Kheldarek <psendrowski@gmail.com>
2016-11-08 15:36:08 +0100

    Initial commit

---
+++ src/com/ps/put/od/cert/generator/CertGenerator.java
@@ -0,0 +1,7 @@
+package com.ps.put.od.cert.generator;
+
+/**
+ * Created by Kheldar on 2016-11-08.
+ */
+public class CertGenerator {
+}
---
+++ src/com/ps/put/od/cert/validator/CertValidator.java
@@ -0,0 +1,7 @@
+package com.ps.put.od.cert.validator;
+
+/**
+ * Created by Kheldar on 2016-11-08.
+ */
+public class CertValidator {
+}
